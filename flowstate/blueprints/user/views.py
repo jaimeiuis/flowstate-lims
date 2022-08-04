f0549887c8809fa507723206794c6b3f6237d00f
jaimeiuis <jaimeljuarez33@gmail.com>
2022-08-04 15:59:06 -0400

    basic user functionality

--- flowstate/blueprints/user/views.py
+++ flowstate/blueprints/user/views.py
@@ -13,6 +13,8 @@ from flask_login import (
     logout_user
 )
 
+from lib.safe_next_url import safe_next_url
+
 from flowstate.blueprints.user.forms import LoginForm
 from flowstate.blueprints.user.models import User
 from flowstate.blueprints.user.decorators import anonymous_required
@@ -27,7 +29,21 @@ def login():
     form = LoginForm(next=request.args.get('next'))
 
     if form.validate_on_submit():
-        pass
+        u = User.find_by_identity(request.form.get('identity'))
+
+        if u and u.authenticated(password=request.form.get('password')):
+            if login_user(u, remember=True) and u.is_active():
+                u.update_activity_tracking(request.remote_addr)
+
+                next_url = request.form.get('next')
+                if next_url:
+                    return redirect(safe_next_url(next_url))
+
+                return redirect(url_for('user.settings'))
+            else:
+                flash('This account has been disabled.', 'error')
+        else:
+            flash('Identity or password is incorrect.', 'error')
 
     return render_template('user/login.html', form=form)
 
